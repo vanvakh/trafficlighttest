d3968c5950c9e46cee9881f53a25436cc51f67c7
vanvakh <vanya.vakhrushev@gmail.com>
2020-08-18 23:17:33 +0300

    Basic requirements fulfilled

--- postsapp/servises.py
+++ postsapp/servises.py
@@ -1,4 +1,5 @@
 import requests, json
+from django.db import DatabaseError, transaction
 
 from .forms import CustomUserForm, PostForm
 from .models import CustomUser, Post
@@ -10,14 +11,12 @@ POSTS_URL = 'http://jsonplaceholder.typicode.com/posts'
 def clear_database():
     CustomUser.objects.all().delete()
 
-def get_users():
-    return (CustomUser.objects.all())
-
 def get_posts():
     return (Post.objects.all())
 
 def load_users_data():
     users_json = get_json(USERS_URL)
+    users_list = []
     for user in users_json:
         form = CustomUserForm(
             data={
@@ -39,10 +38,13 @@ def load_users_data():
             }
         )
         if form.is_valid():
-            form.save()
+            user_instance = form.save(commit=False)
+            users_list.append(user_instance)
+    CustomUser.objects.bulk_create(users_list)
 
 def load_posts_data():
     posts_json = get_json(POSTS_URL)
+    posts_list = []
     for post in posts_json:
         form = PostForm(
             data={
@@ -53,7 +55,9 @@ def load_posts_data():
             }
         )
         if form.is_valid():
-            form.save()
+            post_instance = form.save(commit=False)
+            posts_list.append(post_instance)
+    Post.objects.bulk_create(posts_list)
 
 def get_json(url):
     request = requests.get(url)
